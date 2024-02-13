d6e983e52acb90f20e31c91abbbc712bfeea8448
Antonio Cipriano <antonio@MacBook-Air-154.local>
2024-02-12 22:16:05 -0500

    Adding JS to the project.

---
+++ HW4/Q5.js
@@ -0,0 +1,74 @@
+const cats = [
+    {
+        name: 'Charlie',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lily',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Coco',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Oreo',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Luna',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Milo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lola',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Leo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Willow',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Bella',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Max',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Cleo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lucy',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Daisy',
+        adoptionStatus: 'available'
+    },
+];
+
+
+const result = [];
+for (let cat of cats) {
+    if (cat.adoptionStatus === 'available') {
+        result.push(cat.name);
+    }
+}
+
+let sentence = "All of these cats are available and ready to be adopted: ";
+
+    const lastCat = result.pop();
+    sentence += result.join(", ") + ", and " + lastCat + ".";
+
+
+console.log(sentence);
